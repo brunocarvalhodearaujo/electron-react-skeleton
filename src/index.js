3441e03b6c9d707644139b0fff359918ab24285d
Bruno <bruno@Mini-de-Hostnet.mz.digirati.com.br>
2018-03-07 13:58:24 -0300

    project start

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+  <div>
+    <p>Node: {process.versions.node}</p>
+    <p>Chromium: {process.versions.chrome}</p>
+    <p>Electron: {process.versions.electron}</p>
+  </div>
+)
+
+ReactDOM.render(<App />, document.querySelector('root'))
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { dependencies } = require('./package.json')
+
+module.exports = {
+  target: 'electron-renderer',
+  entry: path.join(__dirname, 'src'),
+  externals: Object.keys(dependencies || {}),
+  output: {
+    filename: 'index.js',
+    path: path.join(__dirname, 'app'),
+    publicPath: './',
+    // https://github.com/webpack/webpack/issues/1114
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)?$/,
+        loaders: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true
+            }
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [{
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              // If you are having trouble with urls not resolving add this setting.
+              // See https://github.com/webpack-contrib/css-loader#url
+              url: false,
+              minimize: true,
+              sourceMap: false
+            }
+          }
+          ]
+        })
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'file?name=[name].[ext]'
+      }
+    ]
+  },
+  /**
+   * Determine the array of extensions that should be used to resolve modules.
+   */
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+      '.json',
+      '.css'
+    ],
+    modules: [
+      'node_modules'
+    ]
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: process.env.NODE_ENV || 'production',
+      ELECTRON_ENV: process.env.ELECTRON_ENV || 'production'
+    }),
+    new ExtractTextPlugin('style.css'),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      inject: 'body',
+      hash: true,
+      minify: {
+        removeAttributeQuotes: true
+      }
+    })
+  ]
+}
